649889822f295bc9a0763b5df857019fb2e9272d
onigra <3280467rec@gmail.com>
2017-03-09 16:38:43 +0900

    stderrも出力する

--- lib/tsudura/provisioner/ansible/command.rb
+++ lib/tsudura/provisioner/ansible/command.rb
@@ -4,6 +4,7 @@ module Tsudura::Provisioner
       def self.exec(config)
         Open3.popen3(CommandGenerator.new(config).generate) do |i, o, e, w|
           o.each { |line| puts line }
+          e.each { |line| puts line }
         end
       end
     end
