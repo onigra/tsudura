e0199a4a4905dd2aa1a55f31019569e31f25399b
onigra <3280467rec@gmail.com>
2017-03-09 18:23:51 +0900

    コマンドはExecuter経由で実行する

--- lib/tsudura/provisioner/ansible/command.rb
+++ lib/tsudura/provisioner/ansible/command.rb
@@ -2,11 +2,7 @@ module Tsudura::Provisioner
   module Ansible
     class Command
       def self.exec(config)
-        Open3.popen3(CommandGenerator.new(config).generate) do |_, stdout, stderr, wait_thr|
-          stdout.each { |line| puts line }
-          stderr.each { |line| puts line }
-          raise ::Tsudura::Errors::ProvisioningFailed unless wait_thr.value.success?
-        end
+        Tsudura::Provisioner::Executer.run CommandGenerator.new(config).generate
       end
     end
   end
