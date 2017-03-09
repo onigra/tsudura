8fbb61de13e4e13f6d71cffdeffabeb11d4fa145
onigra <3280467rec@gmail.com>
2017-03-09 17:27:00 +0900

    使わない変数はアンダースコアに

--- lib/tsudura/provisioner/ansible/command.rb
+++ lib/tsudura/provisioner/ansible/command.rb
@@ -2,7 +2,7 @@ module Tsudura::Provisioner
   module Ansible
     class Command
       def self.exec(config)
-        Open3.popen3(CommandGenerator.new(config).generate) do |i, o, e, w|
+        Open3.popen3(CommandGenerator.new(config).generate) do |_, o, e, w|
           o.each { |line| puts line }
           e.each { |line| puts line }
           raise ::Tsudura::Errors::ProvisioningFailed unless w.value.success?
