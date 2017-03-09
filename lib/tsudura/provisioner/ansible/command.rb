669f9519442f0d84ef7e7f18d05c7a56c59fcc75
onigra <3280467rec@gmail.com>
2017-03-09 16:38:06 +0900

    短くした

--- lib/tsudura/provisioner/ansible/command.rb
+++ lib/tsudura/provisioner/ansible/command.rb
@@ -3,7 +3,7 @@ module Tsudura::Provisioner
     class Command
       def self.exec(config)
         Open3.popen3(CommandGenerator.new(config).generate) do |i, o, e, w|
-          o.each do |line| puts line end
+          o.each { |line| puts line }
         end
       end
     end
